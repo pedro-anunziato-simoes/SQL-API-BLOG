a9cd1716b6cce423c4aab9992238f3e204a7bb51
Pedro Henrique <pedro.anunziato.simoes@gmail.com>
2024-09-22 19:23:33 -0300

    Adicionando a View da Lista de usuarios e do cadastro de novo usuario

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import mysql from "mysql2/promise";
 
 const app = express();
 
-// Configura EJS como a engine de renderização de templates
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
@@ -15,11 +14,10 @@ const connection = mysql.createPool({
     database: "unicesumar"
 });
 
-// Middleware para permitir dados no formato JSON
 app.use(express.json());
-// Middleware para permitir dados no formato URLENCODED
-app.use(express.urlencoded({ extended: true }));
 
+app.use(express.urlencoded({ extended: true }));
+//Categorias
 app.get('/categories', async function (req: Request, res: Response) {
     const [rows] = await connection.query("SELECT * FROM categories");
     return res.render('categories/index', {
@@ -46,5 +44,42 @@ app.post("/categories/delete/:id", async function (req: Request, res: Response) 
 
     res.redirect("/categories");
 });
+
+
+//Usuarios
+app.get("/users", async function (req: Request, res: Response) {
+    const [rows] = await connection.query("SELECT * FROM users");
+    return res.render('users/index', {
+        users: rows
+    });
+});
+
+app.get("/users/form", async function (req: Request, res: Response) {
+    return res.render("users/form");
+});
+
+app.post("/users/add", async function(req: Request, res: Response) {
+    const body = req.body;
+    if(body.senha == body.confirmarSenha){
+    const insertQuery = "INSERT INTO users (name,email,senha,papel) VALUES (?,?,?,?)";
+    await connection.query(insertQuery, [body.name,body.email,body.senha,body.papel,body.created_at]);
+
+    res.redirect("/users");}
+    else{
+        alert('No rows');
+        setTimeout(function() {
+           res.redirect('/users');
+        }, 1000);
+    }
+});
+
+app.post("/users/:id/delete/", async function (req: Request, res: Response) {
+    const id = req.params.id;
+    const sqlDelete = "DELETE FROM users WHERE id = ?";
+    await connection.query(sqlDelete, [id]);
+
+    res.redirect("/users");
+});
+
 
 app.listen('3000', () => console.log("Server is listening on port 3000"));
